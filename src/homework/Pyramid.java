fb37f601930027830faf505124665656e0809112
andreym <am8311328@mail.ru>
2026-09-11 16:32:54 +0300

    Практические задания. 2

---
+++ src/homework/Pyramid.java
@@ -0,0 +1,19 @@
+package src.homework;
+
+public class Pyramid {
+    public static void main(String[] args){
+        for (int i = 0; i <= 5; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+       
+        for (int i = 4; i >= 0; i--){
+            for (int j = 0; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
