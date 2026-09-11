52d1287e33d7942f14eba9e20830cc7d48ee0cd3
andreym <am8311328@mail.ru>
2026-09-11 15:53:00 +0300

    Практические задачи. 1

---
+++ src/homework/Main.java
@@ -0,0 +1,11 @@
+package src.homework;
+
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i <= 100; i++){
+            if (i % 5 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
