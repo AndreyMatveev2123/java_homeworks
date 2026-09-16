8fa58af3adb98124d956fbc747ebd25ee744fe0c
andreym <am8311328@mail.ru>
2026-09-16 11:39:45 +0300

    Практические задачи. 11

---
+++ src/homework/Cicle.java
@@ -0,0 +1,20 @@
+package src.homework;
+
+public class Cicle {
+    public static void main(String[] args){
+        cicle();
+    }
+
+    private static void cicle(){
+        int cicleNum = 2;
+        while (cicleNum >= 2 && cicleNum <= 10){
+            if (cicleNum % 2 == 0){
+                System.out.println(cicleNum);
+            }    
+            else{
+
+            }   
+        cicleNum++;
+        }        
+    }
+}
