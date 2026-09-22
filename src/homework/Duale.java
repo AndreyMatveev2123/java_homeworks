26c1590b4d3dd1ad78362b0e3adc04db6e5c8f94
andreym <am8311328@mail.ru>
2026-09-22 14:52:41 +0300

    Практические задачи. 15

---
+++ src/homework/Duale.java
@@ -0,0 +1,40 @@
+package src.homework;
+
+import java.util.Scanner;
+public class Duale {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int healthOrk = input.nextInt();
+        int attackOrk = input.nextInt();
+        int healthElf = input.nextInt();
+        int attackElf = input.nextInt();
+
+        int elfHealthCount = healthElf;
+        int orkHealthCount = healthOrk;
+
+        while (true){
+            elfHealthCount -= attackOrk;
+            orkHealthCount -= attackElf;
+            if (elfHealthCount <= 0 && orkHealthCount <=0){
+                System.out.println("Draw!");
+                break;
+            }
+
+            else if (elfHealthCount <= 0){
+                System.out.println("Orc wins!");
+                break;
+                
+            }
+
+            else if (orkHealthCount <= 0){
+                System.out.println("Elf wins!");
+                break;
+            }
+            
+
+            }
+        }
+         
+    }
+
