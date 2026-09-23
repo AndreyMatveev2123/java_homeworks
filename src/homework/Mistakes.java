4fabcad2dd5456634b2a16e58d1ac4af7dc3b792
andreym <am8311328@mail.ru>
2026-09-23 17:09:09 +0300

    Практические задачи. 21

---
+++ src/homework/Mistakes.java
@@ -0,0 +1,32 @@
+package src.homework;
+
+
+/*
+public static void main(String[] args) {
+public class Main {
+System.out.println(result)
+       int value = add(7, 8)
+       int result = add(value, 15)
+          }
+int result = (x + y)
+   private static int add( int x, int y) {
+             return result;
+   }
+}
+ */
+
+public class Mistakes {
+    public static void main(String[] args) {
+        int value = add(7, 8);
+        int result = add(value, 15);
+
+        System.out.println(result);
+    }
+
+   private static int add(int x, int y) {
+        int result = (x + y);
+        return result;
+   }
+}
+
+
