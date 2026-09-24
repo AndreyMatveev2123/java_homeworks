9c0f90b1b08f4f7c885211ca675b727fbf5a41ef
andreym <am8311328@mail.ru>
2026-09-24 18:16:19 +0300

    Практические задачи. 26

--- src/homework/MultiplicationTable.java
+++ src/homework/MultiplicationTable.java
@@ -3,7 +3,7 @@ package src.homework;
 public class MultiplicationTable {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++){
-            System.out.print(i + " ");
+            System.out.print(i);
             for (int j = 1; j <= 9; j++){
                 System.out.print(" " + (i * j));
             }
@@ -11,12 +11,3 @@ public class MultiplicationTable {
         }
     }
 }
-            /* 
-            for (int j = 1; j <= 9; j++){
-                System.out.print(" " +  (i * j));
-            }
-            System.out.println();
-        }
-    }
-}
-*/
